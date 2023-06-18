756db9b6fbe2341837e0b5509bcce72dd757ce17
Egor Tirskih <egorhapik@mail.ru>
2023-06-18 21:41:28 +0800

    1 lesson

--- aiogrambot.py
+++ aiogrambot.py
@@ -1,1 +1,16 @@
-print('Hello world!')
+from aiogram import Bot, Dispatcher, executor, types
+
+TOKEN_API = '6131064408:AAFMdm6OirXjxh_9rt7jr9TLy6k7GIs554Q'
+
+bot = Bot(TOKEN_API)
+
+dp = Dispatcher(bot)
+
+
+@dp.message_handler()
+async def echo(message: types.Message):
+    await message.answer(text=message.text)
+    
+
+if __name__ == "__main__":
+    executor.start_polling(dp)
